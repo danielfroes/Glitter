ea5ac95b8ba91aa4cd5cb253399ed6e4f148010d
Daniel Fróes <danielfroes15@gmail.com>
2020-04-29 08:15:18 -0300

    dog cube

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -47,7 +47,7 @@ void Shader_TextureScript(unsigned int ID)
 void Shader_RotateScript(unsigned int ID)
 {
 	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 	
 	
 	glm::mat4 view = glm::mat4(1.0f);
@@ -78,40 +78,89 @@ int main()
 	WindowHolder windowHolder;
 	windowHolder.createWindow(1000, 600, "Pagina do Daniel");
 
+
+	glEnable(GL_DEPTH_TEST);
+
 	//############################################################################################################
-	float bgPanelVert[] = {
-		-1.0f, -1.0f, 0.0f, 0.8f, 0.6f, 0.6f, 0.0f, 0.0f,// left - bot
-		1.0f, -1.0f, 0.0f,  0.7f, 0.6f, 0.6f, 5.0f, 0.0f,//right - bot
-		-1.0f, 1.0f, 0.0f, 0.4f, 0.6f, 0.6f, 0.0f, 5.0f,// left - top
-		1.0f, 1.0f, 0.0f, 0.1f, 0.6f, 0.6f, 5.0f, 5.0f//right - top
-	};
-	unsigned int bgPanelIndex[] = {
-		0, 1, 2,
-		1, 2, 3
-	};
+	//float bgPanelVert[] = {
+	//	-1.0f, -1.0f, 0.0f, 0.8f, 0.6f, 0.6f, 0.0f, 0.0f,// left - bot
+	//	1.0f, -1.0f, 0.0f,  0.7f, 0.6f, 0.6f, 5.0f, 0.0f,//right - bot
+	//	-1.0f, 1.0f, 0.0f, 0.4f, 0.6f, 0.6f, 0.0f, 5.0f,// left - top
+	//	1.0f, 1.0f, 0.0f, 0.1f, 0.6f, 0.6f, 5.0f, 5.0f//right - top
+	//};
+	//unsigned int bgPanelIndex[] = {
+	//	0, 1, 2,
+	//	1, 2, 3
+	//};
 
 	//**Decidir oq fazer para se tem cor ou não
-	float vertices1[] = { // Retangulo (Pos-3, Color-3, Texture-2)
-	-0.25f,  0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, // top right
-	-0.25f, -0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
-	-0.75f, -0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
-	-0.75f,  0.25f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f// top left 
-	};
-	unsigned int indices1[] = {  // note that we start from 0!
-		1, 0, 2,   // first triangle
-		2, 3, 0    // second triangle
+	
+	float cubeVert[] = {
+		// Front face
+		-0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f,// top right
+		-0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,// top left 
+
+		// Back face
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		-0.5f, 0.5f, -0.5f,  1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,// top right
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,// top left 
+
+		// Top face
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		-0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f,// top right
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,// top left 
+
+		// Bottom face
+		-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,// top right
+		-0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,// top left 
+
+		// Right face
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,// top right
+		0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f,// top left 
+
+		// Left face
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f, 0.0f, 0.0f,// bottom left
+		-0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
+		-0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f,  1.0f, 1.0f,// top right
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f,// top left 
 	};
 
 
-	float vertices2[] = { //Triangulo
-	 -0.25f,  -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, 0.0f, 0.0, // bottom left
-	 0.75f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,// bottom right
-	 0.25f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f, 1.0f // top
+
+	unsigned int cubeIndex[] = {
+		0, 1, 2, 0, 2, 3,    // front
+		4, 5, 6, 4, 6, 7,    // back
+		8, 9, 10, 8, 10, 11,   // top
+		12, 13, 14, 12, 14, 15,   // bottom
+		16, 17, 18, 16, 18, 19,   // right
+		20, 21, 22, 20, 22, 23,   // left
 	};
 
-	unsigned int indices2[] = {  // note that we start from 0!
-		0, 1, 2,   // first triangle
+
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
+
+
+
 
 	//############################################################################################################
 	Texture dogTex{ "C:/Users/danie/Documents/OpenGL/Glitter/Textures/dog.jpg"};
@@ -139,14 +188,15 @@ int main()
 	};
 
 	Model modelsArr[] = {
-		Model{bgPanelVert, sizeof(bgPanelVert), bgPanelIndex, sizeof(bgPanelIndex)},
-		Model{vertices1, sizeof(vertices1), indices1, sizeof(indices1), dogShader},//retangulO
-		Model{vertices2, sizeof(vertices2), indices2, sizeof(indices2), blinkingShader},//triangulo
+		//Model{bgPanelVert, sizeof(bgPanelVert), bgPanelIndex, sizeof(bgPanelIndex)},
+		Model{cubeVert, sizeof(cubeVert), cubeIndex, sizeof(cubeIndex), rotateShader}
 	};
 
 	int numModels = sizeof(modelsArr) / sizeof(Model);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	
 
 	RenderLoop(windowHolder, modelsArr, numModels);
 
@@ -164,8 +214,8 @@ int RenderLoop(WindowHolder windowHolder, Model models[] , int numModels)
 
 		//rendering commands//**talvez colocor no windowHolder
 		glClearColor(0.8f, 0.8f, 1.0f, 1.0f); //set the color that will clear the screen
-		glClear(GL_COLOR_BUFFER_BIT); //clear the color buffer
-
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the color buffer
+	 	
 		//
 
 		for(int i = 0; i < numModels; i ++)
