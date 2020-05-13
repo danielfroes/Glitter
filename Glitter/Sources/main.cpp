67e6334537b78707498845bbae9d378d07160f8b
Daniel Fróes <danielfroes15@gmail.com>
2020-05-13 08:28:08 -0300

    camera com zoom, mouse input and key input in main

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -13,10 +13,78 @@
 
 
 
+// Váriáveis globais
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f); //origin
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f); //front vec
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f); //up vec
+
+float lastX = 400, lastY = 300;
+
+
+float deltaTime = 0.0f;
+
+float lastFrame = 0.0f;
+
+float  yaw = -90.0f, pitch = 0.0f;
+
+float fov = 45.0f;
+
+
+
 int RenderLoop(WindowHolder windowHolder, Model models[], int numModels);
-void processInput(GLFWwindow* window);
+void processInput();
 
 
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates range from bottom to top
+
+	lastX = xpos;
+
+	lastY = ypos;
+
+	const float sensitivity = 0.05f;
+
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	direction.y = sin(glm::radians(pitch));
+	direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(direction);
+
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+
+	
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	else if (fov >= 45.0f)
+		fov = 45.0f;
+
+	std::cout << "fov: " << fov << std::endl;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Shaders scripts
 void Shader_BlinkingScript(unsigned int ID)
 {
 	float timeValue = glfwGetTime();
@@ -32,48 +100,67 @@ void Shader_TextureScript(unsigned int ID)
 {
 	unsigned int uniformLocation = glGetUniformLocation(ID, "ourTexture");
 	glUniform1i(uniformLocation, 0);
-	//uniformLocation = glGetUniformLocation(ID, "ourTexture2");
-	//glUniform1i(uniformLocation, 1);
-
-	//float timeValue = glfwGetTime();
-	//float value = (sin(timeValue) / 2.0f) + 0.5f; // goes 1 to 0 back and forth
-	//
-	//uniformLocation = glGetUniformLocation(ID, "interpolationValue");
-	//glUniform1f(uniformLocation, value);
 }
 
 
 
-void Shader_RotateScript(unsigned int ID)
+void Shader_TransformScript(unsigned int ID)
 {
-	if (Input::GetKey(GLFW_KEY_ENTER))
-		std::cout << "input detectado" << std::endl;
+	
 
 	glm::mat4 model = glm::mat4(1.0f);
-	model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-	
-
-	
-	glm::mat4 view = glm::mat4(1.0f);
-	//translating in the reverse direction to simulate the camera
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-	glm::mat4 projection;
-	//args: FOV angle, aspect ratio, near plane, far plane 
-	projection = glm::perspective(glm::radians(45.0f), 1000.0f / 600.0f, 0.1f, 100.0f);
-
+	model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 	unsigned int uniformLocation = glGetUniformLocation(ID, "model");
 	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
 	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(model));
 
+
+
+
+	glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 	uniformLocation = glGetUniformLocation(ID, "view");
 	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
 	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(view));
 
+
+	glm::mat4 projection;
+	//args: FOV angle, aspect ratio, near plane, far plane 
+	projection = glm::perspective(glm::radians(fov), 1000.0f / 600.0f, 0.1f, 100.0f);
 	uniformLocation = glGetUniformLocation(ID, "projection");
 	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
 	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(projection));
 }
+
+
+void Shader_TransformScript2(unsigned int ID)
+{
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(80.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+	model = glm::translate(model, glm::vec3(-1.0f, 0.5f, 1.0f));
+	unsigned int uniformLocation = glGetUniformLocation(ID, "model");
+	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
+	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+
+	glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+	view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	uniformLocation = glGetUniformLocation(ID, "view");
+	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
+	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(view));
+
+
+	glm::mat4 projection;
+	//args: FOV angle, aspect ratio, near plane, far plane 
+	projection = glm::perspective(glm::radians(fov), 1000.0f / 600.0f, 0.1f, 100.0f);
+	uniformLocation = glGetUniformLocation(ID, "projection");
+	//args: uniform location in shader program, how many matrices sending, transpose flag, matrix datas
+	glUniformMatrix4fv(uniformLocation, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 int main()
 {
@@ -177,16 +264,25 @@ int main()
 						NULL
 	};
 
-	Shader rotateShader{
+	Shader transformShader{
 						"C:/Users/danie/Documents/OpenGL/Glitter/Shaders/Transform/transform.vert",
 						"C:/Users/danie/Documents/OpenGL/Glitter/Shaders/Transform/transform.frag",
 						dogTex,
-						Shader_RotateScript
+						Shader_TransformScript
 	};
+
+	Shader transformShader2{
+						"C:/Users/danie/Documents/OpenGL/Glitter/Shaders/Transform/transform.vert",
+						"C:/Users/danie/Documents/OpenGL/Glitter/Shaders/Transform/transform.frag",
+						dogTex,
+						Shader_TransformScript2
+	};
+
 
 	Model modelsArr[] = {
 		//Model{bgPanelVert, sizeof(bgPanelVert), bgPanelIndex, sizeof(bgPanelIndex)},
-		Model{cubeVert, sizeof(cubeVert), cubeIndex, sizeof(cubeIndex), rotateShader}
+		Model{cubeVert, sizeof(cubeVert), cubeIndex, sizeof(cubeIndex), transformShader},
+		Model{cubeVert, sizeof(cubeVert), cubeIndex, sizeof(cubeIndex), transformShader2}
 	};
 
 	int numModels = sizeof(modelsArr) / sizeof(Model);
@@ -205,13 +301,27 @@ int main()
 int RenderLoop(WindowHolder windowHolder, Model models[] , int numModels)
 {
 
+	glfwSetInputMode(windowHolder.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(windowHolder.getWindow(), mouse_callback);
+	glfwSetScrollCallback(windowHolder.getWindow(), scroll_callback);
+
+
 
 	Input::SetWindow(&windowHolder);
 
 	while (!glfwWindowShouldClose(windowHolder.getWindow()))
 	{
+
+
+
+		if (Input::GetKey(GLFW_KEY_ESCAPE))
+		{
+			glfwSetWindowShouldClose(windowHolder.getWindow(), true);
+		}
 		//input
-		/*processInput(windowHolder.getWindow());*/
+		processInput();
+
+
 
 		//rendering commands//**talvez colocor no windowHolder
 		glClearColor(0.8f, 0.8f, 1.0f, 1.0f); //set the color that will clear the screen
@@ -230,6 +340,12 @@ int RenderLoop(WindowHolder windowHolder, Model models[] , int numModels)
 		//check and calls events and swap buffers
 		glfwSwapBuffers(windowHolder.getWindow());	//swaps the color buffer (buffer that contains the colors values for each pixel in GLFW's window) 
 		glfwPollEvents();	//checks if any input event are triggered, and calls the corresponding functions		
+
+
+		//Calculate the time of the last frame
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 	}
 
 
@@ -242,23 +358,34 @@ int RenderLoop(WindowHolder windowHolder, Model models[] , int numModels)
 
 
 
-void processInput(GLFWwindow* window)
+void processInput()
 {
 
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-
-
-
 	//** implementar toogle
-	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+	if (Input::GetKey(GLFW_KEY_Z))
 	{
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	}
-	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+	if (Input::GetKey(GLFW_KEY_X))
 	{
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	}
+
+
+
+	const float cameraSpeed =  2.5f * deltaTime;
+	if (Input::GetKey(GLFW_KEY_W))
+	{
+		cameraPos += cameraSpeed * cameraFront;
+		std::cout << "W detectado" << std::endl;
+	}
+	if (Input::GetKey(GLFW_KEY_S))
+		cameraPos -= cameraSpeed * cameraFront;
+	if (Input::GetKey(GLFW_KEY_D))
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (Input::GetKey(GLFW_KEY_A))
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
 }
+
+
